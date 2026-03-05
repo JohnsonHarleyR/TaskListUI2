0c6985a6e9211f5c0745142f7058596462c60f8d
brian <brian@PHY-REMOTE-429>
2026-03-05 02:22:50 -0600

    Removed React logo.

--- task-list-ui2/src/App.jsx
+++ task-list-ui2/src/App.jsx
@@ -1,7 +1,5 @@
 import { useContext, useState } from 'react'
 import { TaskContext } from './TaskContext'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import TaskArea from './components/TaskArea/TaskArea'
 
@@ -21,9 +19,6 @@ function App() {
         :
         <div>
           <TaskArea />
-          <a href="https://vite.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
         </div>
     }
       
